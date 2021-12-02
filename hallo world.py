36a928912f5b89e89592214dc77d5df0a9d69d9d
Yingrui Li <yingrui.li>
2021-12-02 22:29:13 +0100

    Yingrui

--- hallo world.py
+++ hallo world.py
@@ -1,13 +1,5 @@
 import numpy as np
 import scipy.io as scio
 print("Hallo world!")
-
-x = scio.loadmat('sarcos_inv.mat')
-print(x)
-data = scio.loadmat('sarcos_inv.mat')['sarcos_inv']
-print(data)
-(np.random.shuffle(data))
-print(data.shape)
-xs_train = data[:][:21]
-x = [[1,2,3],[3,4,5],[6,7,8]]
-print(x)
+a = np.array(0)
+print(a.size)
