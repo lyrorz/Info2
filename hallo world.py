7e81920515c7042f38358cee86b8089526d44413
Yingrui Li <yingrui.li>
2021-12-03 23:36:59 +0100

    Yingrui

--- hallo world.py
+++ hallo world.py
@@ -1,4 +1,11 @@
+import collections
+
 import numpy as np
 import scipy.io as scio
 print("Hallo world!")
-
+x = collections.defaultdict(list)
+print(x[1])
+x = np.array([1])
+y = np.array([2])
+z = np.array([[3]])
+print(np.vstack((x,y,z)))
