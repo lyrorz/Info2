b767ca3e1598b8be2243e8b8d33bb40cc15100b9
Yingrui Li <yingrui.li>
2021-12-03 11:51:46 +0100

    Yingrui

---
+++ my_poly_features.py
@@ -0,0 +1,18 @@
+def my_poly_features(xs, degree):
+    """Generates polynomial features from given data
+
+    The polynomial features should include monomials (i.e., x_i, x_i**2 etc)
+    and interaction terms (x_1*x_2 etc), but no repetitions.
+    The order of the samples should not be changed through the transformation.
+
+    Arguments
+    xs      2d numpy array of shape (N,D) containing N samples of dimension D
+    degree  Maximum degree of polynomials to be considered
+
+    Returns
+    An (N,M) numpy array containing the transformed input
+    """
+    # Your implementation
+    pass
+
+# TODO: 手动实现
