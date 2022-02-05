213a32fac7467875b764f9eca0774dc9742163f1
Yingrui Li <yingrui.li>
2022-02-05 12:32:48 +0100

    Yingrui

--- hallo world.py
+++ hallo world.py
@@ -1,10 +1,29 @@
 import collections
-
 import numpy as np
 import scipy.io as scio
 from matplotlib import pyplot as plt
-np.random.seed(1234)
-n = np.random.random(1)
-print(n)
-x = np.array([[1,2],[3,4]])
-print(x.sum(axis=1))
+import matplotlib.image as mpimg
+from pathlib import Path
+
+def funktion(list,num):
+    n = len(list)
+    left = 0
+    right = n-1
+    m = (left+right)//2
+    while left < right-1:
+        if list[m] < num:
+            left = m
+        else:
+            right = m
+        m = (left+right)//2
+        print(left , right)
+    if list[left] >= num:
+        m = left
+    list = list[0:m+1]+[num]+list[m+1:]
+
+    return list
+
+a = [1,2,3,4,5]
+num = 3
+a = funktion(a,num)
+#print(a)
